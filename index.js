e30287974eae5c37f4e3e6d64c6670d632d83714
ririblathmerhen <mongjja106@gmail.com>
2019-04-27 19:22:14 +0900

    19.04.27 파싱 수업 완료

--- index.js
+++ index.js
@@ -1,16 +1,64 @@
-let http = require('http');
-let fs = require('fs');
+//module을 불러온다.
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const request = require('request');
+const cheerio = require('cheerio');
+const bodyParser = require('body-parser');
 
-let server = http.createServer( function(req, res) { 
-    res.setHeader('Content-Type', 'text/html');
-    res.statusCode = 200;
+let app = express(); //let app = new express();
 
-    let file = fs.createReadStream("./index.html");
-    file.on("open", function() {
-        file.pipe(res);
+app.set('port', 12000);
+app.set('views', path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+
+//그냥 받아오지 못하는 .ejs의 경로들을 오류없이 가져오게 해준다.
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json()); //미들웨어로 바디파서를 사용함.
+app.use(bodyParser.urlencoded({extended:true}));
+
+//슬래쉬가 올때 main.ejs로 보내줌
+app.all('/', function(req, res) {
+    res.render('main', {msg: 'Welcome To Express4'});
+});
+
+app.get('/top20', function(req, res) {
+    request("https://www.naver.com", function(err, response, body){ //res가 아닌 response로 한 이유는 위에 res랑 겹쳐서임
+        let list = [];
+        $ = cheerio.load(body);
+
+        let top20 = $(".ah_roll_area > .ah_l .ah_item .ah_k");
+        for(let i = 0; i < top20.length; i++) {
+            let msg = $(top20[i]).text();
+            list.push(msg);
+        }
+
+        res.render('top', {msg:'네이버 실시간 급 상승 겁색어!', list:list});
     });
 });
 
-server.listen(12000, function() {
-    console.log("서버가 12000번 포트에서 실행중 입니다.");
+app.get('/search', function(req, res) {
+    res.render('search', {list:list = undefined});
+});
+
+app.post('/search', function(req, res) {
+    let word = req.body.word;
+    let url = "https://search.naver.com/search.naver?sm=top_hty&fbm=0&ie=utf8&query=" + word;
+    request(url, function(err, response, body){ //res가 아닌 response로 한 이유는 위에 res랑 겹쳐서임
+        let list = [];
+        $ = cheerio.load(body);
+
+        let result = $(".sp_website .type01 > li dt > a:first-child");
+        for(let i = 0; i < result.length; i++) {
+            let msg = $(result[i]).text();
+            list.push(msg);
+        }
+
+        res.render('search', {msg:'검색 결과', list:list});
+    });
+});
+
+let server = http.createServer(app);
+server.listen(app.get('port'), function() {
+    console.log(`Express 엔진이 ${app.get('port')}에서 실행중`);
 });
